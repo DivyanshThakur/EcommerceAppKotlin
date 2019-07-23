4b01b26de28c6f9010bbc67bafa425073f02f775
Divyansh Thakur <divyansh2140@gmail.com>
2019-07-23 16:40:45 +0530

    Added Brand Product List

--- app/src/main/java/com/example/ecommerceappkotlin/EProductAdapter.kt
+++ app/src/main/java/com/example/ecommerceappkotlin/EProductAdapter.kt
@@ -1,27 +1,44 @@
 package com.example.ecommerceappkotlin
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.e_product_row.view.*
 
 class EProductAdapter(var context: Context, var arrayList: ArrayList<EProduct>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
+        var productView = LayoutInflater.from(context).inflate(R.layout.e_product_row,parent, false)
 
-
-
+        return ProductViewHolder(productView)
     }
 
     override fun getItemCount(): Int {
-
-
-
+        return arrayList.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
+        (holder as ProductViewHolder).initializeUiComponents(arrayList[position].id,
+            arrayList[position].name,arrayList[position].price,arrayList[position].pictureName)
+
+    }
 
 
+    inner class ProductViewHolder(pView: View) : RecyclerView.ViewHolder(pView) {
+        fun initializeUiComponents(id : Int, name: String, price : Int, picName : String) {
+            itemView.txtId.text = id.toString()
+            itemView.txtName.text = name
+            itemView.txtPrice.text = price.toString()
+
+            var picURL = "http://192.168.43.186/OnlineStoreApp/osimages/"
+            picURL = picURL.replace(" ", "%20")
+
+            Picasso.get().load(picURL + picName).into(itemView.imgProduct)
+        }
     }
 
 
--- app/src/main/java/com/example/ecommerceappkotlin/FetchEProductsActivity.kt
+++ app/src/main/java/com/example/ecommerceappkotlin/FetchEProductsActivity.kt
@@ -4,10 +4,12 @@ import android.app.AlertDialog
 import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_fetch_eproducts.*
 
 class FetchEProductsActivity : AppCompatActivity() {
 
@@ -16,6 +18,8 @@ class FetchEProductsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_fetch_eproducts)
 
         val selectedBrands : String = intent.getStringExtra("BRAND")
+
+        txtBrandName.text = "Products of $selectedBrands"
 
         var productsList = ArrayList<EProduct>()
 
@@ -33,6 +37,11 @@ class FetchEProductsActivity : AppCompatActivity() {
                 response.getJSONObject(jsonObject).getString("name"),
                 response.getJSONObject(jsonObject).getInt("price"),
                 response.getJSONObject(jsonObject).getString("picture")))
+
+
+            val pAdapter = EProductAdapter(this@FetchEProductsActivity,productsList)
+            productsRV.layoutManager = LinearLayoutManager(this@FetchEProductsActivity)
+            productsRV.adapter = pAdapter
 
 
         }, Response.ErrorListener { error ->
